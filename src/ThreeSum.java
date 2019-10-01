6521473a253319d94bc9fa5fe7b757ba98af4c2b
Marie <Marie.Sallman@trojans.dsu.edu>
2019-10-01 18:51:01 -0500

    Created ThreadCpuStopWatch and implemented it in ThreeSum

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -1,9 +1,28 @@
+import java.lang.management.ManagementFactory;
+
+import java.lang.management.ThreadMXBean;
+
+import java.io.*;
+
 public class ThreeSum {
+
+    static ThreadMXBean bean = ManagementFactory.getThreadMXBean( );
 
     //Setting max and min values for the random ints in the array
     //Having a larger MINVALUE seems to result in a greater chance there will be sets found that = 0.
-    static long MAXVALUE = 1000;
-    static long MINVALUE = -10000;
+    static long MAXVALUE = 10000;
+    static long MINVALUE = -100000;
+    static int numberOfTrials = 10;
+
+    static int MAXINPUTSIZE  = 1000;
+
+    static int MININPUTSIZE  =  1;
+
+    static String ResultsFolderPath = "/home/marie/Results/"; // pathname to results folder
+
+    static FileWriter resultsFile;
+
+    static PrintWriter resultsWriter;
 
     static void threeSum(int[] arr, int n)
     {
@@ -40,22 +59,112 @@ public class ThreeSum {
 
     public static void main(String[] args)
     {
-        //Creates an array of random integers of the designated size using the createRandomIntegerList function.
-        int[] arr = createRandomIntegerList(1000);
+        runFullExperiment("ThreeSum-Exp1-ThrowAway.txt");
 
-        //Code currently commented out, displays all numbers to check that the random generation is working
+        runFullExperiment("ThreeSum-Exp2.txt");
+
+        runFullExperiment("ThreeSum-Exp3.txt");
+    }
+
+    static void runFullExperiment(String resultsFileName){
+
+        try {
+
+            resultsFile = new FileWriter(ResultsFolderPath + resultsFileName);
+
+            resultsWriter = new PrintWriter(resultsFile);
+
+        } catch(Exception e) {
+
+            System.out.println("*****!!!!!  Had a problem opening the results file "+ResultsFolderPath+resultsFileName);
+
+            return; // not very foolproof... but we do expect to be able to create/open the file...
+
+        }
+
+
+
+        ThreadCpuStopWatch BatchStopwatch = new ThreadCpuStopWatch(); // for timing an entire set of trials
+
+        ThreadCpuStopWatch TrialStopwatch = new ThreadCpuStopWatch(); // for timing an individual trial
+
+
+
+        resultsWriter.println("#InputSize    AverageTime"); // # marks a comment in gnuplot data
+
+        resultsWriter.flush();
+
+        for(int inputSize=MININPUTSIZE;inputSize<=MAXINPUTSIZE; inputSize*=2) {
+
+            // progress message...
+
+            System.out.println("Running test for input size "+inputSize+" ... ");
+
+
+
+            /* repeat for desired number of trials (for a specific size of input)... */
+
+            long batchElapsedTime = 0;
+
+            // generate a list of randomly spaced integers in ascending sorted order to use as test input
+
+            // In this case we're generating one list to use for the entire set of trials (of a given input size)
+
+            // but we will randomly generate the search key for each trial
+
+
+
+
+
+
+            // instead of timing each individual trial, we will time the entire set of trials (for a given input size)
+
+            // and divide by the number of trials -- this reduces the impact of the amount of time it takes to call the
+
+            // stopwatch methods themselves
+
+            BatchStopwatch.start(); // comment this line if timing trials individually
+
+
+
+            // run the tirals
+
+            for (long trial = 0; trial < numberOfTrials; trial++) {
+
+                //Creates an array of random integers of the designated size using the createRandomIntegerList function.
+                int[] arr = createRandomIntegerList(1000);
+
+                //Code currently commented out, displays all numbers to check that the random generation is working
         /*for(int i = 0; i < arr.length; i++) {
           System.out.println(arr[i] + " ");
         }*/
 
-        //Sets the int n to the length of the array previously generated
-        int n = arr.length;
+                //Sets the int n to the length of the array previously generated
+                int n = arr.length;
 
-        //Run the threeSum function using the randomly generated array and it's length.
-        threeSum(arr, n);
+                //Run the threeSum function using the randomly generated array and it's length.
+
+                threeSum(arr, n);
+
+            }
+
+            batchElapsedTime = BatchStopwatch.elapsedTime(); // *** comment this line if timing trials individually
+
+            double averageTimePerTrialInBatch = (double) batchElapsedTime / (double)numberOfTrials; // calculate the average time per trial in this batch
+
+
+
+            /* print data for this size of input */
+
+            resultsWriter.printf("%12d  %15.2f \n",inputSize, averageTimePerTrialInBatch); // might as well make the columns look nice
+
+            resultsWriter.flush();
+
+            System.out.println(" ....done.");
+
+        }
 
     }
-
 
     //Function to create the random integers that populate the array.
 
